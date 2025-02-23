2379271b80c8168f143c27b9655c8d4128826619
Kateryna Karpiuk <kateryna_karpiuk0203@tntu.edu.ua>
2025-02-23 18:58:50 +0200

    Revert "host"
    This reverts commit cac4ffc77f8f7cf11e51820096ab07a9a95c1b4f.

--- backend/server.cjs
+++ backend/server.cjs
@@ -8,7 +8,7 @@ const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
 mongoose.connect(DB).then(() => console.log('DB connected succsesfull'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+  console.log(`App is running on port ${port}`); 
 });
